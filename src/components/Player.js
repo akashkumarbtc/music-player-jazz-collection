b6d78783a516504f81f230e8a68ced3559ce7f1e
Akashkumar P <akashkumar.p@accord-soft.com>
2021-04-26 22:18:45 +0530

    react music player

---
+++ src/components/Player.js
@@ -0,0 +1,126 @@
+import React, { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+  faPause,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Player = ({
+  isPlaying,
+  setIsPlaying,
+  currentSong,
+  audioRef,
+  setSongInfo,
+  songInfo,
+  songs,
+  setCurrentSong,
+  setSongs,
+}) => {
+  useEffect(() => {
+    const newSongs = songs.map((song) => {
+      if (song.id === currentSong.id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
+  }, [currentSong]);
+  const playSongHandler = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(!isPlaying);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  const dragHandler = (e) => {
+    audioRef.current.currentTime = e.target.value;
+    setSongInfo({ ...songInfo, currentTime: e.target.value });
+  };
+
+  const getTime = (time) => {
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+  };
+
+  const skipTractHandler = async (direction) => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+    if (direction === "skip-forward") {
+      await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+    }
+    if (direction === "skip-back") {
+      if ((currentIndex - 1) % songs.length === -1) {
+        await setCurrentSong(songs[songs.length - 1]);
+        if (isPlaying) audioRef.current.play();
+        return;
+      }
+      await setCurrentSong(songs[(currentIndex - 1) % songs.length]);
+    }
+    if (isPlaying) audioRef.current.play();
+  };
+
+  const tractAnim = {
+    transform: `translateX(${songInfo.animationPercent}%)`,
+  };
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>{getTime(songInfo.currentTime)}</p>
+        <div
+          style={{
+            background: `linear-gradient(to right, ${currentSong.color[0]}, ${currentSong.color[1]})`,
+          }}
+          className="track"
+        >
+          <input
+            onChange={dragHandler}
+            min={0}
+            max={songInfo.duration || 0}
+            value={songInfo.currentTime}
+            type="range"
+          />
+          <div style={tractAnim} className="animate-track"></div>
+        </div>
+        <p>
+          {songInfo.duration
+            ? getTime(songInfo.duration - songInfo.currentTime)
+            : "0.00"}
+        </p>
+      </div>
+      <div className="play-control">
+        <FontAwesomeIcon
+          onClick={() => skipTractHandler("skip-back")}
+          className="skip-back"
+          size="2x"
+          icon={faAngleLeft}
+        />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          className="play"
+          size="2x"
+          icon={isPlaying ? faPause : faPlay}
+        />
+        <FontAwesomeIcon
+          onClick={() => skipTractHandler("skip-forward")}
+          className="skip-forward"
+          size="2x"
+          icon={faAngleRight}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Player;
---
+++ src/data.js
@@ -0,0 +1,128 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Playful Obsession",
+      artist: "C Y G N",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/d12927eedcc2f5afba2ab049a4a1ea46c2266ae3-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#E4A6F0", "#7DACE2"],
+      active: true,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=14982",
+    },
+    {
+      name: "Jazz Cabbage",
+      artist: "Ian Ewing",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#A74949", "#C5B8C2"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=6492",
+    },
+    {
+      name: "You and I",
+      artist: "Melodiesinfonie",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/0e5bb63f838ff92eeac142aae944e9f678df13c9-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#1F487C", "#352E6F"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10336",
+    },
+    {
+      name: "Murmuration",
+      artist: "Blue Wednesday",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/12/33a2a875828118a3ff260638a88362936104879a-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#B7A6D6", "#805B7D"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=1226",
+    },
+    {
+      name: "Sails",
+      artist: "Aylior Strehlow",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#A74949", "#C5B8C2"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=5971",
+    },
+    {
+      name: "Lost Soul",
+      artist: "C Y G N",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/d12927eedcc2f5afba2ab049a4a1ea46c2266ae3-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#E4A6F0", "#7DACE2"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=14988",
+    },
+    {
+      name: "Back When Marmots Used to ",
+      artist: "Mr. Käfer",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/0e5bb63f838ff92eeac142aae944e9f678df13c9-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#1F487C", "#352E6F"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10324",
+    },
+    {
+      name: "Day One",
+      artist: "Blue Wednesday",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/12/33a2a875828118a3ff260638a88362936104879a-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#B7A6D6", "#805B7D"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11230",
+    },
+    {
+      name: "Cental Coast",
+      artist: "Ian Ewing",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#A74949", "#C5B8C2"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=6493",
+    },
+    {
+      name: "Naked Sunday",
+      artist: "C Y G N",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/d12927eedcc2f5afba2ab049a4a1ea46c2266ae3-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#E4A6F0", "#7DACE2"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=14996",
+    },
+    {
+      name: "Almost Tomorrow",
+      artist: "Liphe",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/0e5bb63f838ff92eeac142aae944e9f678df13c9-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#1F487C", "#352E6F"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10345",
+    },
+    {
+      name: "Déjà Vu",
+      artist: "Blue Wednesday",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/12/33a2a875828118a3ff260638a88362936104879a-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#B7A6D6", "#805B7D"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11228",
+    },
+  ];
+}
+
+export default chillHop;
